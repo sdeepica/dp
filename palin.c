42d5f7366f27de6f26fda4c0e470adb89bf06ac2
sdeepica <sdeeps1028@gmail.com>
2016-12-22 12:21:25 +0530

    Add files via upload

---
+++ palin.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,r,sum=0,temp;
+printf("enter the number:");
+scanf("%d",&n);
+temp=n;
+while(n>0)
+{
+r=n%10;
+sum=(sum*10)+r;
+n=n/10;
+}
+if(temp==sum)
+printf("palindrome number");
+else
+printf("not a palindrome number");
+}
