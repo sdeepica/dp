2d0279f846fc4d4042d58fa7e76f4bfb4a1e5299
sdeepica <sdeeps1028@gmail.com>
2016-12-22 15:21:10 +0530

    Add files via upload

---
+++ table.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int i,j=5;
+    printf(" times 5 table\n");
+    for(i=1;i<=10;i++)
+    {
+            printf("%d x %d=%d\n",j,i,j*i);
+    }
+        printf("\n");
+}
